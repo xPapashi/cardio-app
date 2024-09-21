a2cb7ffd6e44ba01a841bc5eb20a368d3acf97af
xPapashi <dtomkowski1@gmail.com>
2024-09-21 19:31:26 +0200

    changed samesite security for cookie in login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -84,7 +84,7 @@ const loginUser = async (req, res) => {
           if (err) throw err;
 
           console.log("Generated Token:", token);
-          res.cookie("token", token, {sameSite: 'strict'}).json(user);
+          res.cookie("token", token, {sameSite: 'None', secure: true}).json(user);
         }
       );
     }
